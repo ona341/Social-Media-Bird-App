62a2613fcea695915b679602b9a14027fb1dabb9
ona341 <aaghaizu@yahoo.com>
2022-04-21 19:27:16 -0600

    tweet box and Header UI done

--- src/Feed.js
+++ src/Feed.js
@@ -1,8 +1,21 @@
 import React from 'react'
-
+import './Feed.css'
+import TweetBox from './TweetBox'
+import Post from './Post'
 function Feed() {
   return (
-    <div>Feed</div>
+    <div className='feed'>
+      {/* {Header} */}
+      {/* {Posts} */}
+      
+      <div className='feed__header'>
+        <h2>Home</h2>
+        
+        
+        </div>
+        <TweetBox/>
+
+        </div>
   )
 }
 

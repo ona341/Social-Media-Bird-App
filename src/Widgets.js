52d32060245b665bc99577a5da59ffa545a467a6
ona341 <aaghaizu@yahoo.com>
2022-04-22 07:04:45 -0600

    posts done, widgets started

---
+++ src/Widgets.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+
+function Widgets() {
+  return (
+    <div className='widgets'>
+        
+        <h2>Widgets</h2>
+        
+        </div>
+  )
+}
+
+export default Widgets
